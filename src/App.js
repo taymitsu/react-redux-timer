0efe795dc8c1360eb12f3fec6d0fc1c8fc37320b
taymitsu <tmitsumori96@gmail.com>
2023-03-29 11:41:03 -0700

    added new timer input, displaying

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import NewTimer from './components/NewTimer';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
         Hourglass 
+        <NewTimer />
       </div>
     </div>
   );
---
+++ src/components/NewTimer.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addTimer } from '../features/timers/timersSlice';
+
+export default function NewTimer() {
+  const [name, setName] = useState('')
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <input 
+        type='text'
+        placeholder='New Timer Name'
+        name='name'
+        value={name}
+        onChange={(e) => setName(e.target.value)} 
+      />
+      <button
+        onClick={() => dispatch(addTimer(name))}>Save
+      </button>
+      
+    </div>
+  )
+}
