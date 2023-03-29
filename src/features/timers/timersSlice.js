c2e358ec42241fe5d6a3ff2a5e85bec60c5b9b13
taymitsu <tmitsumori96@gmail.com>
2023-03-29 11:19:48 -0700

    added timer actions

---
+++ src/features/timers/timersSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import Timer from './Timer';
+
+const initialState = {
+  value: []
+}
+
+export const timersSlice = createSlice({
+  name: 'timers',
+  initialState, 
+  reducers: {
+    addTimer: (state, action) => {
+      state.value.push(new Timer(action.payload))
+    },
+
+      toggleTimer: (state, action) => {
+        state.value[action.payload].isRunning = !state.value[action.payload].isRunning
+      },
+  },
+})
+
+export const {  } = timersSlice.actions
+export default timersSlice.reducer
+export const { addTimer, toggleTimer } = timersSlice.actions
