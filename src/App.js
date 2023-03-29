888616dedb90000b1496bb581710bca1537035d8
taymitsu <tmitsumori96@gmail.com>
2023-03-29 09:57:49 -0700

    react app + redux

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        Hourglass 
+      </div>
+    </div>
+  );
+}
+
+export default App;
